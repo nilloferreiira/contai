e63f0a27655847ebd2ffa7cdd67a633ffee31824
Danillo Ferreira <nilloferreiira@gmail.com>
2026-09-11 13:32:04 -0300

    feat: add date and money domain primitives

---
+++ src/lib/finance/money.ts
@@ -0,0 +1,5 @@
+export function formatBRL(amountInReais: number): string {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+        .format(amountInReais)
+        .replace(/ /g, ' ')
+}
